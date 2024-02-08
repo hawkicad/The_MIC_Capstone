a3b78241a8fe77d18022607afecc29eacbf54722
scaccol <scaccol@oregonstate.edu>
2024-02-07 16:14:30 -0800

    Sql testing added- successful connection to db

--- soup_test.py
+++ soup_test.py
@@ -10,6 +10,16 @@ soup = BeautifulSoup(html, "html.parser")
 word = soup.find_all('dt')
 definition = soup.find_all('dd')
 
+glossary = {}
+
 # for testing: go through and print every matching word and definition
 for i in range(0, len(word)):
-    print("{}: {}".format(word[i].text.strip(), definition[i].text.strip()))
+    # print("{}: {}".format(word[i].text.strip(), definition[i].text.strip()))
+    glossary[word[i].text.strip()] = definition[i].text.strip()
+
+print("Testing Functionality: Enter word to recieve definition. Type 'Exit' to quit.")
+loop = 1
+while(loop != 0):
+    find = input("Enter Word\n")
+    if (find == "Exit"): loop = 0
+    else: print("The definition of '{}' is:\n".format(find),glossary[find])
---
+++ sql_test.py
@@ -0,0 +1,40 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+# Obtain connection string information from the portal
+config = {
+    'host':'mictestdb.mysql.database.azure.com',
+    'user':'c_hawk7397',
+    'password':'m1(|<3yM0i_i53',
+    'database':'mictestdb',
+    'client_flags': [mysql.connector.ClientFlag.SSL],
+    'ssl_ca': 'DigiCertGlobalRootCA.crt.pem'
+}
+
+# Construct connection string
+try:
+    conn = mysql.connector.connect(**config)
+    print("Connection established")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cursor = conn.cursor()
+
+    # Read data
+    cursor.execute("SELECT * FROM GlossaryEntry;")
+    rows = cursor.fetchall()
+    print("Read",cursor.rowcount,"row(s) of data.")
+
+    # Print all rows
+    for row in rows:
+  	    print("Data row = (%s, %s)" %(str(row[0]), str(row[1])))
+    # Cleanup
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Done.")
